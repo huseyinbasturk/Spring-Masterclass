cc97a7e4e041170b312cdba76c9a6b3d9687a98c
huseyinbasturk <hb.basturk41@gmail.com>
2023-12-09 00:17:25 +0100

    jstl tags

--- todolist/src/main/java/academy/learnprogramming/model/TodoData.java
+++ todolist/src/main/java/academy/learnprogramming/model/TodoData.java
@@ -16,6 +16,8 @@ public class TodoData {
         addItem(new TodoItem("first","first details", LocalDate.now()));
         addItem(new TodoItem("second","second details", LocalDate.now()));
         addItem(new TodoItem("third","third details", LocalDate.now()));
+        addItem(new TodoItem("fourth","fourth details", LocalDate.now()));
+
     }
 
     public List<TodoItem> getItems(){
