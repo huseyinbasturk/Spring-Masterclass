6636d02e1a0ced315bbffa08b896f730e6eba2ed
huseyinbasturk <hb.basturk41@gmail.com>
2023-12-14 23:37:24 +0100

    thymleaf template

--- guess-the-number-game/web/src/main/java/academy/learnprogramming/util/ViewNames.java
+++ guess-the-number-game/web/src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -3,6 +3,8 @@ package academy.learnprogramming.util;
 public class ViewNames {
 
     public static final String HOME = "home";
+    public static final String PLAY = "play";
+    public static final String GAME_OVER = "game-over";
 
     private ViewNames(){}
 }
